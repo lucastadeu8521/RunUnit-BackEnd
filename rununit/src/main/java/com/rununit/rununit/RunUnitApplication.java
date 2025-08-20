30d61b000d48dd8a8b7fb5561d197ee7fc15628f
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-08-19 21:43:27 -0300

    First Commit Test

---
+++ rununit/src/main/java/com/rununit/rununit/RunUnitApplication.java
@@ -0,0 +1,13 @@
+package com.rununit.rununit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunUnitApplication.class, args);
+	}
+
+}
---
+++ rununit/src/test/java/com/rununit/rununit/RunUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rununit.rununit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
