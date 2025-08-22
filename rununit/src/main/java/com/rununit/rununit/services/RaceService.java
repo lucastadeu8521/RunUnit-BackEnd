aec8b5e98a935d93928c4ec7fc47251a61d6f612
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-08-22 12:29:20 -0300

    feat(Service) Create Race Service:
    1. RaceService
    	findAll -> Listar todas as corridas
    	findById
    	insert
    	deleteById

---
+++ rununit/src/main/java/com/rununit/rununit/services/RaceService.java
@@ -0,0 +1,53 @@
+package com.rununit.rununit.services;
+
+import com.rununit.rununit.entities.Race;
+import com.rununit.rununit.repositories.RaceRepository;
+import com.rununit.rununit.services.exceptions.DatabaseException;
+import com.rununit.rununit.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class RaceService {
+
+    @Autowired
+    private RaceRepository repository;
+
+    public List<Race> FindAll(){
+        return repository.findAll();
+    }
+
+    public Race findById(UUID id){
+        Optional<Race> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Race insert(Race obj){
+        return repository.save(obj);
+    }
+
+    public void deleteById(UUID id){
+        try{
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public void UpdateData(Race entity, Race obj){
+        entity.setName(obj.getName());
+        entity.setDescription(obj.getDescription());
+        entity.setLocation(obj.getLocation());
+        entity.setEndTime(obj.getEndTime());
+        entity.setStartTime(obj.getStartTime());
+    }
+}
