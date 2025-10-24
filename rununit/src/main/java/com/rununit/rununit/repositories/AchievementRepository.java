e482185519d40c88dc5443b03325874633f406c5
Francisco Primo <assisbsprimo@gmail.com>
2025-10-23 21:36:23 -0300

    Adiciona novas entidades e repositório de achievements

---
+++ rununit/src/main/java/com/rununit/rununit/repositories/AchievementRepository.java
@@ -0,0 +1,12 @@
+package com.rununit.rununit.repositories;
+
+import com.rununit.rununit.entities.Achievement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AchievementRepository extends JpaRepository<Achievement, Long> {
+    Optional<Achievement> findByName(String name);
+}
