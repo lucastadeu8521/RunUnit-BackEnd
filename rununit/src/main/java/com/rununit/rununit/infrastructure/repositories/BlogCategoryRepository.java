771a14af2cc161f32c1c17d5d85e51b7146e2cb5
CaioGBrayner <caiogomesbrayner@outlook.com>
2025-10-26 09:32:22 -0300

    refactor(repository) Create BlogCategory:
    1 - Create BlogCategoryRepository

--- rununit/src/main/java/com/rununit/rununit/infrastructure/repositories/BlogCategoryRepository.java
+++ rununit/src/main/java/com/rununit/rununit/infrastructure/repositories/BlogCategoryRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.List;
 
 @Repository
 public interface BlogCategoryRepository extends JpaRepository<BlogCategory, Long> {
 
-    Optional<BlogCategory> findByName(String name);
     Optional<BlogCategory> findBySlug(String slug);
-    List<BlogCategory> findByActiveTrue();
+
+    boolean existsBySlug(String slug);
+
+    boolean existsByName(String name);
 }
